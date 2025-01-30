c1293c1f5bec1759208233da97443933758100d6
Hesam Makki <107540794+HMakkiMD@users.noreply.github.com>
2025-01-30 10:49:26 +0000

    Add files via upload

---
+++ Example_SCPs/AIG(b12_2_8x2)_T/MD/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+
+#SBATCH -p node_name
+#SBATCH -N 1
+#SBATCH -n 24
+#SBATCH --time 5-00:00:00
+
+module load apps/gromacs_cuda/2022.0
+
+gmx grompp -f minim.mdp -c AIG(b12_2_8x2)_T_10_1.gro -p AIG(b12_2_8x2)_T_10_1.top -o min.tpr
+
+gmx mdrun -deffnm min -nt 24 -tableb table_corr_d1.xvg table_corr_d2.xvg table_corr_d3.xvg
+
+gmx grompp -f npt.mdp -c min.gro -p AIG(b12_2_8x2)_T_10_1.top -o npt.tpr
+
+gmx mdrun -deffnm npt  -nt 12 -tableb table_corr_d1.xvg table_corr_d2.xvg table_corr_d3.xvg
+
+gmx grompp -f eq_soup.mdp -c npt.gro -p AIG(b12_2_8x2)_T_10_1.top -o 500/500.tpr
+
+gmx mdrun -deffnm 500/500  -nt 24 -tableb table_corr_d1.xvg table_corr_d2.xvg table_corr_d3.xvg
+
